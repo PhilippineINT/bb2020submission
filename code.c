19d1e88f927ea9da1d9092546844081704829df8
PhilippineINT <60892064+PhilippineINT@users.noreply.github.com>
2020-02-10 22:03:43 +0300

    Update code.c

--- code.c
+++ code.c
@@ -5,22 +5,22 @@ int main()
 	printf("Start\n");
     enable_servos(); set_servo_position(front_claw,0);  //set_servo_position(claw_arm,//#); set_servo_position(claw_joint,//#); set_servo_position(claw_claw,//#);
     //wait_for_light();printf("Light detected!\n");
-	while(analog(0)<2900){mav(rwheel,1500); mav(lwheel,1500);} ao(); msleep(200); //forward
-	mav(rwheel,1000); mav(lwheel,1000); msleep(1200); //get to other side of line
-	cmpc(rwheel); while(gmpc(rwheel)<2375){motor(rwheel,60); motor(lwheel,0);} //turn left
-	while(analog(0)<2900){mav(rwheel,1500); mav(lwheel,1500);} //forward
-	ao(); msleep(200); //small sleep
+    while(analog(0)<2900){mav(rwheel,1500); mav(lwheel,1500);} ao(); msleep(200); //forward
+    mav(rwheel,1000); mav(lwheel,1000); msleep(1200); //get to other side of line
+    cmpc(rwheel); while(gmpc(rwheel)<2375){motor(rwheel,60); motor(lwheel,0);} //turn left
+    while(analog(0)<2900){mav(rwheel,1500); mav(lwheel,1500);} //forward
+    ao(); msleep(200); //small sleep
     cmpc(rwheel); enable_servos(); set_servo_position(front_claw,2047); //open front claw
     while(gmpc(rwheel)<1800){mav(rwheel,1150);mav(lwheel,1150);} //forward a specific distance
-	cmpc(rwheel);while(gmpc(rwheel)<2375){motor(rwheel,100); motor(lwheel,0);} //turn left, '90'
-	while(analog(rangefinder)<2500){mav(rwheel,300); mav(lwheel,300);} //material processor
+    cmpc(rwheel);while(gmpc(rwheel)<2375){motor(rwheel,100); motor(lwheel,0);} //turn left, '90'
+    while(analog(rangefinder)<2500){mav(rwheel,300); mav(lwheel,300);} //material processor
     mav(rwheel,500); mav(lwheel,500); msleep(1000); //slowly go towards the processror
-	ao(); enable_servos(); //stop,
-	set_servo_position(front_claw,0); msleep(500); //grasp processor
-	while(analog(0)<2800){mav(rwheel, -1500); mav(lwheel, -1500);} //fastly yank processor, going  until black line
+    ao(); enable_servos(); msleep(200); //stop,
+    set_servo_position(front_claw,0); msleep(500); //grasp processor
+    while(analog(0)<2800){mav(rwheel, -1500); mav(lwheel, -1500);} //fastly yank processor, going  until black line
     ao(); msleep(500); //small sleep
-	cmpc(lwheel); while(gmpc(lwheel)>-2375){motor(lwheel,-100); motor(rwheel,0);} //turn right
-   	cmpc(rwheel); while(gmpc(rwheel)<5800){mav(rwheel,1500); mav(lwheel,1500);} //"forward" to certain position
+    cmpc(lwheel); while(gmpc(lwheel)>-2375){motor(lwheel,-100); motor(rwheel,0);} //turn right
+    cmpc(rwheel); while(gmpc(rwheel)<5800){mav(rwheel,1500); mav(lwheel,1500);} //"forward" to certain position
     cmpc(rwheel); while(gmpc(rwheel)<2000){motor(rwheel,60); motor(lwheel,0);} //turn a specific angle
     mav(rwheel,800); mav(lwheel,800); msleep(450); //set the material processor onto a corner
     ao(); msleep(500); set_servo_position(front_claw,2047); //release the material processor
